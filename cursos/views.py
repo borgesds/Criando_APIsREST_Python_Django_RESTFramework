6b08a047a1a7e73d50b9afc34dc04ef52ec01b48
André Borges <borges.an@hotmail.com>
2023-05-03 19:44:05 -0300

    up Code

--- cursos/views.py
+++ cursos/views.py
@@ -2,6 +2,8 @@ from rest_framework import generics
 from rest_framework.generics import get_object_or_404
 
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
@@ -59,6 +61,12 @@ Utiliza = from rest_framework import viewsets
 class CursosViewSet(viewsets.ModelViewSet):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
+
+    @action(detail=True, methods=['get'])
+    def avaliacoes(self, request, pk=None):
+        curso = self.get_object()
+        serializer = AvaliacaoSerializer(curso.avaliacoes.all(), many=True)
+        return Response(serializer.data)
 
 
 class AvaliacaoViewSet(viewsets.ModelViewSet):
