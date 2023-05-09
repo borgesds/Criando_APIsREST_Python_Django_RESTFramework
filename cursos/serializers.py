d3d34ac8424b9031d82fd026f9edd236e161bb85
André Borges <borges.an@hotmail.com>
2023-05-09 07:09:14 -0300

    up Code

--- cursos/serializers.py
+++ cursos/serializers.py
@@ -20,6 +20,11 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
             'ativo'
         )
 
+    def validate_avaliacao(self, valor):  # validate_xxx campo de valiação
+        if valor in range(1, 6):  # so vai ter notas de 1 a 5
+            return valor
+        raise serializers.ValidationError('A avaliação precisa ser entre 1 e 5')
+
 
 class CursoSerializer(serializers.ModelSerializer):
     # Aqui mandamos todas as avalições para leitura de uma vez
